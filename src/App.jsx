93ef9dfa6ecdf18f74651f37092eb440940f5b6c
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-12 20:43:30 +0700

    Adding PageNotFound component and handle routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
+import Pagenotfound from "./pages/Pagenotfound";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<Pagenotfound />} />
       </Routes>
     </BrowserRouter>
   );
