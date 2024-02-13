516ecaebb6429d806c9d43cca2675173d0fe90eb
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-13 07:19:02 +0700

    include global css like name inde and come styling AppNav component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 export default function HomePage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>HomePage</h1>
       <Link to="/app">Go to the app</Link>
     </div>
