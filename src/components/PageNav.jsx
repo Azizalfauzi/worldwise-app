1bbe3b3717af0e41cbdf0b78fa4a207d8a67882d
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-15 05:26:57 +0700

    updating some styling  button LoginPage

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -16,7 +16,9 @@ export default function PageNav() {
           <NavLink to="/product">Product</NavLink>
         </li>
         <li>
-          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/login" className={style.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
