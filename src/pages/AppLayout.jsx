5f6121fc206d714431aa32329aad26d675a7b231
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-15 05:56:30 +0700

    Modified Map and set In AppLayout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,13 @@
 // import AppNav from "../components/AppNav";
 import SideBar from "../components/SideBar";
+import Map from "../components/Map";
 import styles from "./Applayout.module.css";
 
 export default function AppLayout() {
   return (
     <div className={styles.app}>
       <SideBar />
+      <Map />
     </div>
   );
 }
