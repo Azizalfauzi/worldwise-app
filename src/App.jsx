bbae1bfee45cea99a390609f97fa565d2181f4c3
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-16 05:44:49 +0700

    create new function fetchCities in App component and create useState

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,31 @@ import AppLayout from "./pages/AppLayout";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:9000";
 
 export default function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+        console.log(cities);
+        console.log(isLoading);
+      } catch {
+        alert("There where was an error loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
