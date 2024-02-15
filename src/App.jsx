a2ea35796f76491cc3a9edcc7841d7898aa67b1a
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-16 05:33:08 +0700

    Adding package json-server and create new component CityList

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Pagenotfound from "./pages/Pagenotfound";
 import AppLayout from "./pages/AppLayout";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
 
 export default function App() {
   return (
@@ -15,8 +16,8 @@ export default function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
           <Route path="countries" element={<p>List of Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
