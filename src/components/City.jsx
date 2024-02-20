88988d388e62ae3dc4f3b6d34aa4e60e0834e821
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-20 13:41:21 +0700

    Handle get data param with useParams

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,6 +1,9 @@
+import { useParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 function City() {
+  const { id } = useParams();
+
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -12,7 +15,7 @@ function City() {
   // const { cityName, emoji, notes } = currentCity;
   // const { cityName, emoji, date, notes } = currentCity;
 
-  return <h1>City</h1>;
+  return <h1>City {id}</h1>;
   // return (
   //   <div className={styles.city}>
   //     <div className={styles.row}>
