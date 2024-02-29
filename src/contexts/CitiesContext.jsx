8b517483067f64518f18152d95ebdb15375b8094
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-29 08:40:59 +0700

    feat(CityItemContext,CityItem): Delete City
    - Create service deleteCity
    - Implement in CityItem

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -51,7 +51,21 @@ function CitiesProvider({ children }) {
       const data = await res.json();
       setCities((cities) => [...cities, data]);
     } catch {
-      alert("There where was an error loading data");
+      alert("There where was an error create city");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch {
+      alert("There where was an error deleting city");
     } finally {
       setIsLoading(false);
     }
@@ -65,6 +79,7 @@ function CitiesProvider({ children }) {
         currentCity,
         getCity,
         createCity,
+        deleteCity,
       }}
     >
       {children}
